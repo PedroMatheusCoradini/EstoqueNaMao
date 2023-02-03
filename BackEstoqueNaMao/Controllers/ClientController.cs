492e58eb94a7b9ba029e8d4fe00f64720d0cffce
Pedro Matheus <matt_4.6.8@outlook.com>
2023-02-02 21:30:51 -0300

    Iniciando um crud. Feito Get e Post

--- BackEstoqueNaMao/Controllers/ClientController.cs
+++ BackEstoqueNaMao/Controllers/ClientController.cs
@@ -1,9 +1,33 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using BackEstoqueNaMao.DataContext;
+using BackEstoqueNaMao.Models;
 
 namespace BackEstoqueNaMao.Controllers;
 
 [ApiController]
-public class ClientController
+[Route("api/client")]
+public class ClientController : ControllerBase
 {
+    private readonly Context _context;
 
+    public ClientController(Context context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Client>>> GetAsync()
+    {
+        return await _context.Clients.ToListAsync();
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<Client>> PostAsync(Client client)
+    {
+        await _context.Clients.AddAsync(client);
+        await _context.SaveChangesAsync();
+
+        return Ok();
+    }
 }
